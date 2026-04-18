8946d08a5b25286feaec5b5a2c477c6c8aa68257
吐司Toast <macbook@192.168.2.15>
2026-04-18 10:42:20 +0800

    Initial Commit

---
+++ SuperCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperCalculator
+//
+//  Created by Macbook on 2026/4/18.
+//  Copyright © 2026年 TusiToast. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
